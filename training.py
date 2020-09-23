c6508e29c01a0b7c99405d457658d2d93ce01a80
Mayank Chandak <mayank.chandak97@gmail.com>
2020-09-23 12:31:20 +0530

    Update training.py

--- training.py
+++ training.py
@@ -5,12 +5,11 @@ import numpy as np
 
 training_data_x, training_data_y = generate_training_data()
 model = Sequential()
-model.add(Dense(units=9,input_dim=7)) # yeh sab padh le jaise isme relu and softmax function ki layeer banai h humne K
+model.add(Dense(units=9,input_dim=7))
 model.add(Dense(units=15, activation='relu'))
 model.add(Dense(output_dim=3,  activation = 'softmax'))
 
-model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy']) # phir yahan hamne model ko compile karke uski accuracy nikali 
-''' uske liye hamne adam filter use kiya h and model ko penalize '''
+model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 model.fit((np.array(training_data_x).reshape(-1,7)),( np.array(training_data_y).reshape(-1,3)), batch_size = 256,epochs= 3)
 
 model.save_weights('dnn_model.h5')
